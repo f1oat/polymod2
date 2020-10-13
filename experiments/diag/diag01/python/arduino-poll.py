cde6fdc13242d74d95dfe5bbb107e070e0f8b769
Frederic Rible <f1oat@f1oat.org>
2020-10-13 22:41:50 +0100

    Add analog and digital inputs reporting to Python test script

--- experiments/diag/diag01/python/arduino-poll.py
+++ experiments/diag/diag01/python/arduino-poll.py
@@ -7,14 +7,21 @@ import smbus
 bus = smbus.SMBus(1)    # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
 modules = [ 4 ];
 
+connectionReportingMaxSize = 4*3;
+analogReportingMaxSize = 2*3;
+digitalReportingMaxSize = 2*2;
+
 def testConnections():
 	for tickNum in range(0, 32):
 		bus.write_byte_data(0, 0, tickNum)	# Broadcast, offset=0 means tick message
 	return
 
-def getChanges():
+def configureMessageSize():
+	bus.write_i2c_block_data(0, 32, [connectionReportingMaxSize, analogReportingMaxSize, digitalReportingMaxSize])
+
+def getConnectionChanges():
 	for addr in modules:
-		pdu = bus.read_i2c_block_data(addr, 1, 8) # Offset=1 means connections polling
+		pdu = bus.read_i2c_block_data(addr, 1, connectionReportingMaxSize) # Offset=1 means connections polling
 		ptr = 0
 		while (pdu[ptr] != 0xFF and ptr <= 30):
 			fromModule = pdu[ptr] & 0x7F
@@ -26,6 +33,26 @@ def getChanges():
 			if (connected): print("connected: m%dp%d -> m%dp%d" % (fromModule, fromPort, toModule, toPort))
 			else: print("disconnected: m%dp%d -> m%dp%d" % (fromModule, fromPort, toModule, toPort))
 
+def getAnalogInputChanges():
+	for addr in modules:
+		pdu = bus.read_i2c_block_data(addr, 2, analogReportingMaxSize) # Offset=2 means analog inputs polling
+		ptr = 0
+		while (pdu[ptr] != 0xFF and ptr <= 30):
+			pinId = pdu[ptr]
+			value = (pdu[ptr+1] << 8) + pdu[ptr+2]
+			ptr += 3
+			print("Analog m%dp%d = %d" % (addr, pinId, value))
+
+def getDigitalInputChanges():
+	for addr in modules:
+		pdu = bus.read_i2c_block_data(addr, 3, digitalReportingMaxSize) # Offset=3 means analog inputs polling
+		ptr = 0
+		while (pdu[ptr] != 0xFF and ptr <= 30):
+			pinId = pdu[ptr]
+			value = pdu[ptr+1]
+			ptr += 2
+			print("Digital m%dp%d = %d" % (addr, pinId, value))
+
 if __name__ == "__main__":
 	client = udp_client.SimpleUDPClient('127.0.0.1', 9001)
 	#url = sys.argv[1]
@@ -33,16 +60,16 @@ if __name__ == "__main__":
 	#args = list(map(int, args))
 	#client.send_message(url, args)
 
-	phase = ""
-
 	while True:
 		try:
-			phase = "connnections testing"
-			testConnections()
-			phase = "changes collection"
-			getChanges()
+			configureMessageSize()
+			while True:
+				testConnections()
+				getConnectionChanges()
+				getAnalogInputChanges()
+				getDigitalInputChanges()
 		except OSError:
-			print("I2C error during " + phase)
+			print("I2C error")
 			time.sleep(1);
 
 		time.sleep(1e-3)
